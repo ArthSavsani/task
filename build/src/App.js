f5a96e66e4b26b6bf9d9bcbc2bd8bb22c87104c7
ArthSavsani <79360159+ArthSavsani@users.noreply.github.com>
2022-09-17 15:25:53 +0530

    Add files via upload

---
+++ build/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Task from "./task";
+
+const App = () => {
+  return (
+    <>
+      <Task />
+    </>
+  );
+};
+
+export default App;
--- src/task.jsx
+++ src/task.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import API from "./product";
 import "./task.css";
 
